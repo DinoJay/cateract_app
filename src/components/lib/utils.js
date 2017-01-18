2ac88823b91ec20f3a818b47c58804b63d1957c1
DinoJay <jott88@googlemail.com>
2017-01-18 23:50:25 +0100

    started zoom and pan

--- src/components/lib/utils.js
+++ src/components/lib/utils.js
@@ -1,9 +1,1 @@
-import moment from 'moment';
 
-function dateDiff(a, b, t) {
-  const aDate = moment(a);
-  const bDate = moment(b);
-  return Math.ceil(bDate.diff(aDate, t, true));
-}
-
-export { dateDiff };
